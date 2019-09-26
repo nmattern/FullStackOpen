3d20a4255b699866cef0df3aa7b46378d1dfc331
Storm Mattern <storm.mattern@gmail.com>
2019-09-25 23:41:13 -0500

    Completed practice of parts c and d

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -31,12 +31,14 @@ const Part = props => {
 
 const Total = props => {
   return (
-    <p>
-      Number of exercises{" "}
-      {props.parts[0].exercises +
-        props.parts[1].exercises +
-        props.parts[2].exercises}
-    </p>
+    <>
+      <p>
+        Number of exercises{" "}
+        {props.parts[0].exercises +
+          props.parts[1].exercises +
+          props.parts[2].exercises}
+      </p>
+    </>
   );
 };
 
