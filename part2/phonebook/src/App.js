d9d9959cd9305024f8a88d749d6ac977ce670b70
Storm Mattern <storm.mattern@gmail.com>
2019-09-28 19:39:38 -0500

    Completed up to step 3 of the phonebook exercise

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Person from "./components/Person";
 
-function App() {
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456" },
+    { name: "Ada Lovelace", number: "39-44-5323523" },
+    { name: "Dan Abramov", number: "12-43-234345" },
+    { name: "Mary Poppendieck", number: "39-23-6423122" }
+  ]);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [newFilter, setNewFilter] = useState("");
+
+  const rows = () =>
+    persons.map(person => (
+      <Person key={person.id} name={person.name} number={person.number} />
+    ));
+
+  const addPerson = event => {
+    event.preventDefault();
+    if (persons.some(e => e.name === newName)) {
+      alert(`${newName} is already added to phonebook`);
+    } else {
+      const personObject = {
+        name: newName,
+        number: newNumber,
+        id: persons.length + 1
+      };
+      setPersons(persons.concat(personObject));
+    }
+    setNewName("");
+    setNewNumber("");
+  };
+
+  const handleNameChange = event => setNewName(event.target.value);
+  const handleNumberChange = event => setNewNumber(event.target.value);
+  const handleNameFilter = event => setNewFilter(event.target.value);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Phonebook</h2>
+      <p>
+        filter shown with{" "}
+        <input value={newFilter} onChange={handleNameFilter} />
+      </p>
+      <h2>add a new</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>{rows()}</div>
     </div>
   );
-}
+};
 
 export default App;
